0174f49784cf2261deac20ac0c16afb3eb45b839
yatus <yatupeng@gmail.com>
2019-05-13 09:25:51 +0800

    调整提交

--- chapter1/src/main/java/cn/yatus/chapter1/config/JpaConfig.java
+++ chapter1/src/main/java/cn/yatus/chapter1/config/JpaConfig.java
@@ -1,8 +1,12 @@
 package cn.yatus.chapter1.config;
 
 import cn.yatus.chapter1.App;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.AutoConfigureBefore;
+import org.springframework.boot.autoconfigure.orm.jpa.JpaProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.Database;
@@ -12,15 +16,17 @@ import javax.sql.DataSource;
 
 @Configuration
 public class JpaConfig {
+    @Autowired
+    private JpaProperties jpaProperties;
 
     @Bean
     public LocalContainerEntityManagerFactoryBean entityManagerFactory(DataSource routeDataSource) {
         HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
         vendorAdapter.setDatabase(Database.MYSQL);
         // 是否生成表
-        vendorAdapter.setGenerateDdl(true);
+        vendorAdapter.setGenerateDdl(jpaProperties.isGenerateDdl());
         // 是否显示sql语句
-        vendorAdapter.setShowSql(true);
+        vendorAdapter.setShowSql(jpaProperties.isShowSql());
         LocalContainerEntityManagerFactoryBean factory = new LocalContainerEntityManagerFactoryBean();
         factory.setJpaVendorAdapter(vendorAdapter);
         // 配置扫描的位置
